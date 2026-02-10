fbffaa490157b1760a2adda6fbdc78e4f4f3480c
v1nc3ntn <vincentnguyen0723@gmail.com>
2026-02-10 15:06:12 +0000

    calc.cpp initialisations

---
+++ calc.cpp
@@ -0,0 +1,25 @@
+//begin = calc.cpp ============
+#include <sstream>
+#include <iostream>
+
+int main (int argc , char * argv [] ) {
+int a, b, c;
+
+if(argc != 3 ) returnl;
+
+std::string  sa( argv [1] ) ;
+std::string  sb( argv [2] ) ;
+
+
+std::stringstream ssa( sa ) ;
+std::stringstream ssb( sb ) ;
+
+ssa >> a ;
+ssb >> b ;
+
+c = add ( a , b ) ;
+std::cout << c ;
+
+return 0 ;
+
+}
