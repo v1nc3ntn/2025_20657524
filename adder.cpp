779ccdf73f4068127aeb49774f011e4bacdd7ee0
v1nc3ntn <vincentnguyen0723@gmail.com>
2026-02-10 15:22:04 +0000

    Fix adder.cpp

--- adder.cpp
+++ adder.cpp
@@ -1,17 +1,7 @@
-//begin – adder.h
-// The following two lines prevent adder.h from being included
-// more than once in any source (.cpp) file. If this were to happen
-// it would cause problems in the compile process but it is difficult to
-// prevent manually in large projects. These #ifndef, #define and #endif
-// commands form an "include guard" and are types of compiler directive.
-// The include guard contains an ID for this file "MATHSLIB_ADDER_H", this is
-// arbitrary but must be unique within the project.
+//begin — adder.cpp ———————————————————————
+#include "adder.h"
 
-#ifndef MATHSLIB_ADDER_H
-#define MATHSLIB_ADDER_H
-
-// prototype for our function
-int add(int a, int b);
-
-#endif
-//end – adder.h
+int add( int a, int b ) {
+    return a + b;
+}
+//end — adder.cpp —————————————————————————
