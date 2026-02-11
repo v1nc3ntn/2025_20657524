6c523118308c9d015de324fe43eddebfa8a482d6
v1nc3ntn <vincentnguyen0723@gmail.com>
2026-02-11 11:29:32 +0000

    fix calc.cpp

--- Worksheet3/calc.cpp
+++ Worksheet3/calc.cpp
@@ -1,11 +1,23 @@
+#include <sstream>
 #include <iostream>
 #include "adder.h"
 
-int main() {
-    int a = 2;
-    int b = 3;
+int main(int argc, char* argv[]) {
+    int a, b, c;
 
-    std::cout << "a + b = " << add(a, b) << std::endl;
+    if (argc != 3) return 1;
+
+    std::string sa(argv[1]);
+    std::string sb(argv[2]);
+
+    std::stringstream ssa(sa);
+    std::stringstream ssb(sb);
+
+    ssa >> a;
+    ssb >> b;
+
+    c = add(a, b);
+    std::cout << c;
 
     return 0;
 }
