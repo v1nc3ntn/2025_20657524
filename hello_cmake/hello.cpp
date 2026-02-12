b023adfcd28add919e73420b8597adf1f91c2219
v1nc3ntn <vincentnguyen0723@gmail.com>
2026-02-12 11:43:01 +0000

    Worksheet 4: static and shared CMake library build completed

---
+++ hello_cmake/hello.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+//begin - hello.cpp
+int main (int argc, char *argv[] )
+{
+std::cout << "Hello Planet Earth!" << std::endl;
+std::cout << "Tobenna is too moisttt!" << std::endl;
+return 0;
+}
+// end - hello.cpp
